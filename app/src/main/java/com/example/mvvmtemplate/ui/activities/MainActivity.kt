47e5346d12289faff3f9d1d419d0ac8ef8d9a9b9
dastageer <dastageerb44@gmail.com>
2022-04-18 05:40:35 +0500

    activity binding

--- app/src/main/java/com/example/mvvmtemplate/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/mvvmtemplate/ui/activities/MainActivity.kt
@@ -3,13 +3,21 @@ package com.example.mvvmtemplate.ui.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.mvvmtemplate.R
+import com.example.mvvmtemplate.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity()
 {
+    var _binding:ActivityMainBinding?=null
+    private val binding get() = _binding!!
+
+
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding  = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
 
 
 
